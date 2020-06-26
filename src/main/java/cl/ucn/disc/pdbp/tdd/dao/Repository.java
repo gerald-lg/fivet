b3596224d98c221ecfadd5c93f2efdf6e059a797
gerald-lg <gerald.lopez@alumnos.ucn.cl>
2020-06-26 20:18:14 -0300

    feat(DAO): implements crud in the generic repository

---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/dao/Repository.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Gerald Lopez Gutiérrez <gerald.lopez@alumnos.ucn.cl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cl.ucn.disc.pdbp.tdd.dao;
+
+import java.util.List;
+
+/**
+ * Capa de acceso a  datos.
+ * @author Gerald Lopez
+ *
+ * @param <T> tipo de dato
+ * @param <K> tipo de llave del elemento
+ */
+public interface Repository<T, K> {
+
+  /**
+   * Obtiene el listado de todos los T en el repositorio.
+   * @return la lista de T
+   */
+  List<T> findAll();
+
+  /**
+   * Obtiene un T a traves de su identificador.
+   * @param id a buscar
+   * @return T con la id.
+   */
+  T findById(K id);
+
+  /**
+   * Crea un T en el repositorio.
+   * @param objeto a crear
+   * @return true si se creo correctamente.
+   */
+  boolean create(T objeto);
+
+  /**
+   * Actualiza los datos de T en el repositorio.
+   * @param objeto a actualizar
+   * @return true si se actualizo satisfactoriamente.
+   */
+  boolean update(T objeto);
+
+  /**
+   * Borra un T dado su identificador.
+   * @param id de T a eliminar.
+   * @return true si se elimino correctamente.
+   */
+  boolean delete(K id);
+
+
+}
---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/dao/RepositoryOrmLite.java
@@ -0,0 +1,141 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Gerald Lopez Gutiérrez <gerald.lopez@alumnos.ucn.cl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cl.ucn.disc.pdbp.tdd.dao;
+
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Implementacion generica de {@link Repository}.
+ * @author Gerald Lopez
+ * @param <T> tipo de dato
+ * @param <K> tipo del Id.
+ */
+public final class RepositoryOrmLite<T, K> implements Repository<T, K> {
+
+  private final Dao<T, K> theDao;
+
+  /**
+   * Constructor.
+   * @param connectionSource conexion con  ORM.
+   * @param theClazz para usar como fuente.
+   */
+  public RepositoryOrmLite(ConnectionSource connectionSource, Class<T> theClazz) {
+
+    try {
+      theDao = DaoManager.createDao(connectionSource, theClazz);
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+  }
+
+  /**
+   * Obtiene el listado de todos los T en el repositorio.
+   *
+   * @return la lista de T
+   */
+  @Override
+  public List<T> findAll() {
+
+    try {
+      return theDao.queryForAll();
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+  }
+
+  /**
+   * Obtiene un T a traves de su identificador.
+   *
+   * @param id a buscar
+   * @return T con la id.
+   */
+  @Override
+  public T findById(K id) {
+
+    try {
+      return theDao.queryForId(id);
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+  }
+
+  /**
+   * Crea un T en el repositorio.
+   *
+   * @param objeto a crear
+   * @return true si se creo correctamente.
+   */
+  @Override
+  public boolean create(T objeto) {
+
+    try {
+      return theDao.create(objeto) == 1;
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+  }
+
+  /**
+   * Actualiza los datos de T en el repositorio.
+   *
+   * @param objeto a actualizar
+   * @return true si se actualizo satisfactoriamente.
+   */
+  @Override
+  public boolean update(T objeto) {
+
+    try {
+      return theDao.update(objeto) == 1;
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+  }
+
+  /**
+   * Borra un T dado su identificador.
+   *
+   * @param id de T a eliminar.
+   * @return true si se elimino correctamente.
+   */
+  @Override
+  public boolean delete(K id) {
+
+    try {
+      return theDao.deleteById(id) == 1;
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+  }
+}
